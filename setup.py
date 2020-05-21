268593cc8b60c4917701fef0c1d60f2ac7cb1914
tcdude <tc@tizilogic.com>
2020-05-21 18:02:56 +0200

    Bump version for new release

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ OPTIONS = {'apk': {'debug': None,
 
 setup(
     name='Simple Solitaire',
-    version='0.3.10',
+    version='0.3.11',
     description='An ad free, simple solitaire game',
     author='tcdude',
     author_email='tizilogic@gmail.com',
